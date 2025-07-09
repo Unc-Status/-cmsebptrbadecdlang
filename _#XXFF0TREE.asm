3d89721be768b352592c385d62c9e33a038cc3f8
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-09 14:19:30 -0500

    Update _#XXFF0TREE.asm

--- _#XXFF0TREE.asm
+++ _#XXFF0TREE.asm
@@ -1,8 +1,35 @@
 ;================
 ; for token tree
 ;================
+;[name] [variable] [value]
 
 section .bss
 global _xxffotree
 _xxffotree:
+
+_defint db 'defaultint'
+_int db 'int'
+_floating db 'floating'
+_boolean db 'boolean' ;bool uses 0 for false and 1 for true
+_char db 'char'
+_color db 'color'
+_red db 'red'
+_green db 'green'
+_yellow db 'yellow' ;tel comp slow duwn
+_warning db 'warning'
+_repactive db 'repactive'
+_freevar db 'freevar'
+_freefunc db 'freefunc'
+_for db 'for'
+_handle db 'handle'
+_typesize db 'typesize'
+_charptr db 'char->'
+_intptr db 'int->'
+_floatptr db 'float->'
+_boolptr db 'boolean->'
+_failedfor db 'failedfor'
+_globalaccess db 'globalaccess:' ;pblc
+_armoredaccess db 'armoredaccess:' ;prtced
+_hiddenaccess db 'hiddenaccess' ;prvte
+
 
